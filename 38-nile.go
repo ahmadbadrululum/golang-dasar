453ce060c4066f814d2752aef2d6d5ebf880219d
badrules <ahmad.badrul@vcgamers.com>
2023-01-05 07:16:46 +0700

    nile

---
+++ 38-nile.go
@@ -0,0 +1,26 @@
+ackage main
+
+import "fmt"
+
+func NewMap(name string) map[string]string {
+	if name == "" {
+		return nil
+	} else {
+		return map[string]string{
+			"name": name,
+		}
+	}
+}
+
+func main() {
+	var person map[string]string = NewMap("Eko")
+
+	if person == nil {
+		fmt.Println("Data Kosong")
+	}else {
+		fmt.Println(person)
+	}
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
