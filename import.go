07b7b818e991ca1bd6c73f4c8b97c3bc1a450d1b
badrules <ahmad.badrul@vcgamers.com>
2023-01-06 07:25:28 +0700

    package dan import

---
+++ import.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-dasar2/helper"
+)
+
+func main() {
+	helper.SayHello("Eko")
+	// fmt.Println(helper.Application)
+	// helper.sayGoodbye("Eko") // error
+	fmt.Println("cuk")
+	// fmt.Println(helper.version) // error
+}
